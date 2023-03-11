c27ca2e148edfa3c9ea420052da45f92bbf25777
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-10 22:32:13 -0300

    exercicio cidades do litoral sul

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("cidades");
+Console.WriteLine("Santos");
+Console.WriteLine("Cubatão");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
